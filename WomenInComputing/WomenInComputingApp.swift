c08ac5edcd18c64b328893513e27bfd098499535
Bob Beachill <bbeachill@icloud.com>
2025-03-03 21:30:09 +0000

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Robert Beachill on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
